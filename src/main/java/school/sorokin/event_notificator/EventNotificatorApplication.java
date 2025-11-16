4a63da9a1c3291f112a9e5bda6fb5b3fbe6b71b6
anton <4ack90@gmail.com>
2025-11-16 13:11:25 +0300

    init commit

---
+++ src/main/java/school/sorokin/event_notificator/EventNotificatorApplication.java
@@ -0,0 +1,13 @@
+package school.sorokin.event_notificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNotificatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventNotificatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/sorokin/event_notificator/EventNotificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sorokin.event_notificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
